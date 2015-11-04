ff8aa3a2f3d398294d2990bdc93be7255f28c060
Youcef Mammar <youcef.mammar@fr.ibm.com>
2015-11-04 16:50:54 +0100

    fix issues with svg build. Fixes #29 Refs #30

--- tests/unit/svg.js
+++ tests/unit/svg.js
@@ -7,11 +7,14 @@ define([
 		return require.config({
 			context: "svg" + context++,
 			baseUrl: "../../../requirejs-dplugins",
-			paths: {lie: "../lie", "requirejs-text": "../requirejs-text/"},
+			paths: {lie: "../lie", "requirejs-text": "../requirejs-text/", "requirejs-domready": "../requirejs-domready/"},
 			config: {
 				"svg": {
 					layersMap: {
-						"tests/unit/resources/svg/never-loaded.svg": "tests/unit/resources/svg/sprite.svg"
+						"tests/unit/resources/svg/never-loaded.svg": {
+							redirectTo:  "tests/unit/resources/svg/sprite.svg",
+							id: "never-loaded"
+						}
 					}
 				}
 			}
@@ -34,7 +37,7 @@ define([
 			contextRequire([
 				"svg!tests/unit/resources/svg/icon1.svg",
 				"svg!tests/unit/resources/svg/icon2.svg"
-			], dfd.callback(function () {
+			], dfd.callback(function (id1, id2) {
 				var spriteContainer = document.getElementById(CONTAINER_ID);
 				assert.isNotNull(spriteContainer, "Sprite was correctly created");
 				var icon1 = spriteContainer.querySelector("symbol#icon1"),
@@ -45,6 +48,8 @@ define([
 				assert.isNotNull(icon2, "icon2 was correctly added");
 				assert.isNull(inexistentIcon, "inexistent-icon was not found as expected");
 				assert.strictEqual(symbols.length, 2, "total number of symbols found is correct");
+				assert.strictEqual(id1, "icon1", "id1 is correct");
+				assert.strictEqual(id2, "icon2", "id1 is correct");
 			}));
 		},
 		"Checking svgs are correctly added to sprite": function () {
@@ -70,10 +75,12 @@ define([
 			contextRequire([
 				"svg!tests/unit/resources/svg/icon1.svg",
 				"svg!tests/unit/resources/svg/icon1.svg"
-			], dfd.callback(function () {
+			], dfd.callback(function (id1, id2) {
 				var spriteContainer = document.getElementById(CONTAINER_ID);
 				var svgs = spriteContainer.querySelectorAll("symbol#icon1");
 				assert.strictEqual(svgs.length, 1, "Icon was not added twice");
+				assert.strictEqual(id1, "icon1", "Id1 is correct");
+				assert.strictEqual(id2, "icon1", "Id2 is correct");
 			}));
 		},
 		"Checking svg defined in sprite can't be reloaded": function () {
@@ -81,12 +88,80 @@ define([
 			var contextRequire = getContextRequire();
 			contextRequire([
 				"svg!tests/unit/resources/svg/never-loaded.svg"
-			], dfd.callback(function () {
+			], dfd.callback(function (id) {
 				var spriteContainer = document.getElementById(CONTAINER_ID);
 				var svgs = spriteContainer.querySelectorAll("symbol#never-loaded"),
 					sprite = spriteContainer.querySelectorAll("symbol#sprite");
 				assert.strictEqual(svgs.length, 0, "Icon was not loaded");
 				assert.strictEqual(sprite.length, 1, "Sprite was loaded instead");
+				assert.strictEqual(id, "never-loaded", "Id is correct");
+			}));
+		},
+		"Loading an icon without an id": function () {
+			var dfd = this.async();
+			var contextRequire = getContextRequire();
+			contextRequire([
+				"svg!tests/unit/resources/svg/icon-without-id.svg"
+			], dfd.callback(function (id) {
+				var spriteContainer = document.getElementById(CONTAINER_ID);
+				var svgs = spriteContainer.querySelectorAll("symbol");
+				assert.strictEqual(svgs.length, 1, "Icon without id was loaded");
+				var idAttr = svgs[0].getAttribute("id");
+				assert.strictEqual(idAttr, null, "Icon without id gets no id attribute in the sprite");
+				assert.strictEqual(id, null, "svg! returns null when fetching a icon without id");
+			}));
+		},
+		"Loading an icon with an empty id": function () {
+			var dfd = this.async();
+			var contextRequire = getContextRequire();
+			contextRequire([
+				"svg!tests/unit/resources/svg/icon-with-empty-id.svg"
+			], dfd.callback(function (id) {
+				var spriteContainer = document.getElementById(CONTAINER_ID);
+				var svgs = spriteContainer.querySelectorAll("symbol");
+				assert.strictEqual(svgs.length, 1, "Icon with empty id was loaded");
+				var idAttr = svgs[0].getAttribute("id");
+				assert.strictEqual(idAttr, "", "Icon without id gets no id attribute in the sprite");
+				assert.strictEqual(id, "", "svg! returns null when fetching a icon without id");
+			}));
+		},
+		"Loading an icon without a viewBox": function () {
+			var dfd = this.async();
+			var contextRequire = getContextRequire();
+			contextRequire([
+				"svg!tests/unit/resources/svg/icon-without-viewBox.svg"
+			], dfd.callback(function () {
+				var spriteContainer = document.getElementById(CONTAINER_ID);
+				var svgs = spriteContainer.querySelectorAll("symbol");
+				assert.strictEqual(svgs.length, 1, "Icon without viewBox was loaded");
+				var viewBoxAttr = svgs[0].getAttribute("viewBox");
+				assert.strictEqual(viewBoxAttr, null, "Icon without viewBox gets no viewBox attribute in the sprite");
+			}));
+		},
+		"Loading an icon with an empty viewBox": function () {
+			var dfd = this.async();
+			var contextRequire = getContextRequire();
+			contextRequire([
+				"svg!tests/unit/resources/svg/icon-with-empty-viewBox.svg"
+			], dfd.callback(function () {
+				var spriteContainer = document.getElementById(CONTAINER_ID);
+				var svgs = spriteContainer.querySelectorAll("symbol");
+				assert.strictEqual(svgs.length, 1, "Icon with empty viewBox was loaded");
+				var viewBoxAttr = svgs[0].getAttribute("viewBox");
+				assert.strictEqual(viewBoxAttr, "", "Icon with empty viewBox gets an empty viewBox attribute in the sprite");
+			}));
+		},
+		"Loading an icon with a lowercase viewbox": function () {
+			var dfd = this.async();
+			var contextRequire = getContextRequire();
+			contextRequire([
+				"svg!tests/unit/resources/svg/icon-with-lowercase-viewbox.svg"
+			], dfd.callback(function (id) {
+				var spriteContainer = document.getElementById(CONTAINER_ID);
+				var svgs = spriteContainer.querySelectorAll("symbol");
+				assert.strictEqual(svgs.length, 1, "Icon with lowercase viewbox was loaded");
+				var viewBoxAttr = svgs[0].getAttribute("viewBox");
+				assert.strictEqual(viewBoxAttr, "0 0 32 32", "lowercase viewbox attribute was converted to camel case viewBox");
 			}));
 		}
 	});
